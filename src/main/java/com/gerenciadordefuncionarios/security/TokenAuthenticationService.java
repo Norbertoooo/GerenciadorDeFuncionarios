999806e1af88d3082d25b8c34350e9ef13ea7194
Vítor Norberto <vithor.norberto@gmail.com>
2019-09-17 10:10:01 -0300

    Altera expiration time para 10 min

--- src/main/java/com/gerenciadordefuncionarios/security/TokenAuthenticationService.java
+++ src/main/java/com/gerenciadordefuncionarios/security/TokenAuthenticationService.java
@@ -14,8 +14,8 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 public class TokenAuthenticationService {
 
-    // EXPIRATION_TIME = 10 dias
-    static final long EXPIRATION_TIME = 860_000_000;
+    // EXPIRATION_TIME =  10 minutos
+    static final long EXPIRATION_TIME = 600_000;
     static final String SECRET = "MySecret";
     static final String TOKEN_PREFIX = "Bearer";
     static final String HEADER_STRING = "Authorization";
