b377be7aea1206eb9a1052a4c3708ea431dc449d
Vítor Norberto <vithor.norberto@gmail.com>
2019-09-19 00:03:56 -0300

    Spring database configuration

--- src/main/java/com/gerenciadordefuncionarios/GerenciadorDeFuncionariosApplication.java
+++ src/main/java/com/gerenciadordefuncionarios/GerenciadorDeFuncionariosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class GerenciadorDeFuncionariosApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(GerenciadorDeFuncionariosApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/gerenciadordefuncionarios/config/DatabaseConfiguration.java
@@ -0,0 +1,37 @@
+package com.gerenciadordefuncionarios.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("dev")
+public class DatabaseConfiguration {
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/postgres");
+        dataSourceBuilder.username("postgres");
+        dataSourceBuilder.password("0000");
+        return dataSourceBuilder.build();
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.POSTGRESQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+}
