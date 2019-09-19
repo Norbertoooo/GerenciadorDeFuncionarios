41c4e21a4d0521d4e927fb3345de459cf1db6029
Vítor Norberto <vithor.norberto@gmail.com>
2019-09-19 00:04:50 -0300

    Altera endpoint para listar funcionarios.

--- src/main/java/com/gerenciadordefuncionarios/resources/FuncionarioResource.java
+++ src/main/java/com/gerenciadordefuncionarios/resources/FuncionarioResource.java
@@ -3,8 +3,10 @@ package com.gerenciadordefuncionarios.resources;
 import com.gerenciadordefuncionarios.domain.Funcionario;
 import com.gerenciadordefuncionarios.services.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -17,7 +19,7 @@ public class FuncionarioResource {
     @Autowired
     public FuncionarioService funcionarioService;
 
-    @GetMapping(value = "listaDeFuncionarios")
+    @GetMapping(value = "listarFuncionarios")
     public ResponseEntity<List<Funcionario>> listarFuncionario(){
         List<Funcionario> listaDeFuncionarios = funcionarioService.listarFuncionarios();
         return ResponseEntity.ok(listaDeFuncionarios);
@@ -35,15 +37,27 @@ public class FuncionarioResource {
     }
     @DeleteMapping(value = "excluirFuncionario/{id}")
     public ResponseEntity<Void> excluirFuncionario(@PathVariable long id){
+        try{
         funcionarioService.excluirFuncionario(id);
         return ResponseEntity.noContent().build();
+        }
+        catch (Exception exception) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "id Not Found");
+        }
     }
 
     @PutMapping(value = "atualizarFuncionario/{id}")
     public ResponseEntity<Void> atualizarFuncionario(@RequestBody Funcionario funcionario, @PathVariable long id){
-        funcionario.setId(id);
-        funcionarioService.atualizarFuncionario(funcionario);
-        return ResponseEntity.noContent().build();
+        try {
+            funcionario.setId(id);
+            funcionarioService.atualizarFuncionario(funcionario);
+            return ResponseEntity.noContent().build();
+        }
+        catch (Exception exception) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Id not found", exception);
+        }
     }
 
 
